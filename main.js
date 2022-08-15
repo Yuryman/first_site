b8c0a457780b715d3a44f3d05b4d76df0d42e620
Yuryman <manskov@ukr.net>
2022-08-15 20:38:55 +0000

    19Практика_ч1

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hi');
+'use strict';
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+const personalMuvieDB = {
+count: numberOfFilms,
+movies:{},
+actors: {},
+genres:[],
+privat: false
+};
+const a= prompt('Один из последних просмтренных фильмов?', ''),
+      b= prompt('На сколько оцените его?', ''),
+      c= prompt('Один из последних просмтренных фильмов?', ''),
+      d= prompt('На сколько оцените его?', '');
+
+      personalMuvieDB.movies[a]=b;
+      personalMuvieDB.movies[c]=d;
+      console.log(personalMuvieDB);
+
